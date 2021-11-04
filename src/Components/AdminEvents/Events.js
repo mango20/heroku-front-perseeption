5edb85de8960e0bc5a9d123b8368f2c3d93d1fdc
unknown <mangacharmainelilan20@gmail.com>
2021-11-04 18:32:02 +0800

    make it better

--- src/Components/AdminEvents/Events.js
+++ src/Components/AdminEvents/Events.js
@@ -411,16 +411,12 @@ function Events() {
               {" "}
               Submit{" "}
             </button>
-            {previewSource && (
-              <img
-                src={previewSource}
-                alt="chosen"
-                style={{ height: "300px" }}
-              />
-            )}
           </form>
         </div>
         <div className="container">
+          {previewSource && (
+            <img src={previewSource} alt="chosen" style={{ height: "300px" }} />
+          )}
           {/* <CloudinaryContext cloudName="dlvt2lnkh">
             <div>
               <Image
