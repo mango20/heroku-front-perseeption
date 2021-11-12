cea047fe409dffd03dadc25fea330f9e729f768a
unknown <mangacharmainelilan20@gmail.com>
2021-11-13 07:20:51 +0800

    make it better

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -22,36 +22,41 @@ function Login() {
       USER_PASSWORD: password,
     }).then((response) => {
       // alert(response.data.message);
-      if (response.data) {
+      // if (response.data) {
+      //   setloginMessage(response.data.message);
+      //   document.getElementById("bgLoginStats").style.display = "block";
+      //   setTimeout(function () {
+      //     document.getElementById("bgLoginStats").style.display = "none";
+      //   }, 3000);
+      //   console.log(response.data);
+      //   console.log(response.data.result);
+      //   return;
+      // } else {
+      setloginMessage(response.data.message);
+      document.getElementById("bgLoginStats").style.display = "block";
+      setTimeout(function () {
+        document.getElementById("bgLoginStats").style.display = "none";
+      }, 3000);
+      var userDetails = response.data.result;
+      localStorage.setItem("Client", JSON.stringify(userDetails));
+      if (response.data.result[0].USER_TYPE === "Admin") {
         setloginMessage(response.data.message);
         document.getElementById("bgLoginStats").style.display = "block";
         setTimeout(function () {
           document.getElementById("bgLoginStats").style.display = "none";
         }, 3000);
-        console.log(response.data);
-        console.log(response.data.result);
-        return;
+        history.push("/AdminDashboard");
+        window.location.reload();
       } else {
-        var userDetails = response.data.result;
-        localStorage.setItem("Client", JSON.stringify(userDetails));
-        if (response.data.result[0].USER_TYPE === "Admin") {
-          setloginMessage(response.data.message);
-          document.getElementById("bgLoginStats").style.display = "block";
-          setTimeout(function () {
-            document.getElementById("bgLoginStats").style.display = "none";
-          }, 3000);
-          history.push("/AdminDashboard");
-          window.location.reload();
-        } else {
-          setloginMessage(response.data.message);
-          document.getElementById("bgLoginStats").style.display = "block";
-          setTimeout(function () {
-            document.getElementById("bgLoginStats").style.display = "none";
-          }, 3000);
-          history.push("/");
-          window.location.reload();
-        }
+        setloginMessage(response.data.message);
+        document.getElementById("bgLoginStats").style.display = "block";
+        setTimeout(function () {
+          document.getElementById("bgLoginStats").style.display = "none";
+        }, 3000);
+        history.push("/");
+        window.location.reload();
       }
+      // }
     });
   };
   // Axios.defaults.withCredentials = true;
