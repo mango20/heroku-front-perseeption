8859d6c1437144da6a69172f12c1f3d53c434790
unknown <mangacharmainelilan20@gmail.com>
2021-11-13 03:58:20 +0800

    make it better

--- src/Components/AdminMembersList/AdminMemberList.js
+++ src/Components/AdminMembersList/AdminMemberList.js
@@ -630,7 +630,7 @@ function AdminMemberList() {
 
   const [NAME, setNAME] = useState("");
   // const [USER_ID, setUSER_ID] = useState("");
-  const [USERNAME, setUSERNAME] = useState("");
+  // const [USERNAME, setUSERNAME] = useState("");
   const [CITY_ADDRESS, setCITY_ADDRESS] = useState("");
   const [MOTHER_CONTACT, setMOTHER_CONTACT] = useState("");
   const [USER_ID, setUSER_ID] = useState("");
