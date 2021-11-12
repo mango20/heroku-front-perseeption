351b4a081915ed5a3a2ede68072537fc0eb4daa1
unknown <mangacharmainelilan20@gmail.com>
2021-11-13 07:01:10 +0800

    make it better

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -22,12 +22,13 @@ function Login() {
       USER_PASSWORD: password,
     }).then((response) => {
       // alert(response.data.message);
-
       setloginMessage(response.data.message);
-      // document.getElementById("bgLoginStats").style.display = "block";
-      // setTimeout(function () {
-      //   document.getElementById("bgLoginStats").style.display = "none";
-      // }, 3000);
+      document.getElementById("bgLoginStats").style.display = "block";
+      setTimeout(function () {
+        document.getElementById("bgLoginStats").style.display = "none";
+      }, 3000);
+      console.log(response.data);
+      console.log(response.data.result);
       var userDetails = response.data.result;
       localStorage.setItem("Client", JSON.stringify(userDetails));
       if (response.data.result[0].USER_TYPE === "Admin") {
