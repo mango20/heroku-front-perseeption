276356175c1a96744cbf4aec463e7b09e1d5b1f5
unknown <mangacharmainelilan20@gmail.com>
2021-11-15 02:07:07 +0800

    make it better

--- src/Components/AdminAnnouncement/AdminAnnouncement.js
+++ src/Components/AdminAnnouncement/AdminAnnouncement.js
@@ -128,6 +128,18 @@ function AdminAnnouncement() {
             ANNOUNCEMENT_CONTENT: ANNOUNCEMENT_CONTENT,
           }
         );
+        document.getElementById("messageAnnouncementPopUpouter").style.display =
+          "block";
+        document.getElementById("messageAnnouncement_Content").style.display =
+          "block";
+
+        setTimeout(function () {
+          document.getElementById(
+            "messageAnnouncementPopUpouter"
+          ).style.display = "none";
+          document.getElementById("messageAnnouncement_Content").style.display =
+            "none";
+        }, 3000);
         setFileInputState1("");
         setPreviewSource1("");
         setANNOUNCEMENT_TITLE("");
