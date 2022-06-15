fd12888485a051f2b7639c25afc73bdcfc12a630
cmangawfn <mangacharmainelilan20@gmail.com>
2022-06-15 13:28:18 +0800

    update

--- src/Components/AdminAdminList/AdminAdminList.js
+++ src/Components/AdminAdminList/AdminAdminList.js
@@ -172,18 +172,21 @@ function AdminAdminList() {
       document.getElementById("addAdminUsername").value = "";
       setUSER_PASSWORD("");
       document.getElementById("addAdminPassword").value = "";
+      setSelectedFile("");
+      document.getElementById("avatarIcon_").value = "";
 
-      Axios.get("https://perseeption-tromagade.herokuapp.com/AdminList").then(
-        (response) => {
-          setADMIN_LIST(response.data);
-        }
-      );
       document.getElementById("popUpGetMsgAdminAdded").style.display = "block";
 
       setTimeout(function () {
         document.getElementById("popUpGetMsgAdminAdded").style.display = "none";
         // document.getElementById("popUpGetMsgInCont").style.display = "none";
       }, 3000);
+
+      Axios.get("https://perseeption-tromagade.herokuapp.com/AdminList").then(
+        (response) => {
+          setADMIN_LIST(response.data);
+        }
+      );
     }
   };
 
